b72ca46d54df702929353212c79b2259adbd3ff6
f58075956-gif <f58075956@gmail.com>
2026-06-09 21:18:51 -0300

    Change welcome title to 'CRONOS | WELCOME'

--- Roci.lua
+++ Roci.lua
@@ -13,7 +13,7 @@ local function getCharacter()
     return player.Character or player.CharacterAdded:Wait()
 end
 
-local title = ("ZIX DOM | WELCOME ")
+local title = "CRONOS | WELCOME " .. Player.DisplayName .. " (@" .. Player.Name .. ")"
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/f58075956-gif/Sxo/refs/heads/main/W%20UI.txt", true))()
 
