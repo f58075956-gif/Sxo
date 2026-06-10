19bf944b207964695c970b53ad7d5fd95ead6aca
f58075956-gif <f58075956@gmail.com>
2026-06-09 21:51:54 -0300

    Update Roci.lua

--- Roci.lua
+++ Roci.lua
@@ -13,7 +13,7 @@ local function getCharacter()
     return player.Character or player.CharacterAdded:Wait()
 end
 
-local title = "CRONOS | WELCOME " .. game.Players.LocalPlayer.DisplayName
+local title = ("CRONOS | WELCOME ")
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/f58075956-gif/Sxo/refs/heads/main/W%20UI.txt", true))()
 
 local window = library:AddWindow(title, {
