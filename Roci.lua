9df70953417a1de280350b30840122482e063067
f58075956-gif <f58075956@gmail.com>
2026-06-19 20:43:01 -0300

    Update Roci.lua

--- Roci.lua
+++ Roci.lua
@@ -13,7 +13,7 @@ local function getCharacter()
     return player.Character or player.CharacterAdded:Wait()
 end
 
-local title = ("epic script | Hello ")
+local title = (" op script | CRONOS ")
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/f58075956-gif/Sxo/refs/heads/main/W%20UI.txt", true))()
 
